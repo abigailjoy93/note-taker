9ffb478989c4ddba433f99feabcb6747a5ac87ac
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-08-30 20:15:22 -0500

    edit-6

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   const notes = JSON.parse(fs.readFileSync("develop/db/db.json", "utf8"));
   notes.push(newNote)
+});
 
 // Default route for any other request
 app.get("*", (req, res) => {
