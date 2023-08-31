a177d92a7616bc2822bf6c811dca4608dc5963b3
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-08-30 20:51:49 -0500

    debugging-2

--- server.js
+++ server.js
@@ -39,3 +39,58 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Fetch existing notes and render them
+async function getAndRenderNotes() {
+  const response = await fetch("/api/notes");
+  const notes = await response.json();
+  // Render the notes in the left column
+}
+
+getAndRenderNotes();
+
+// Save a new note
+async function saveNote() {
+  const title = document.querySelector(".note-title").value;
+  const text = document.querySelector(".note-textarea").value;
+
+  if (title && text) {
+    const response = await fetch("/api/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, text }),
+    });
+
+    getAndRenderNotes();
+  }
+}
+
+// Serve the notes.html file when the /notes route is accessed
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// Serve the index.html file for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// API endpoint to get existing notes
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  res.json(notes);
+});
+
+// API endpoint to save a new note
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4(); // Generate a unique ID for the new note
+  const notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  notes.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(notes));
+  res.json(newNote);
+});
